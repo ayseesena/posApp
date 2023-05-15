acd5708596def3b3a8a044927eb16fb1fff61348
ayseesena <ayseesena.61@gmail.com>
2023-05-15 14:13:01 +0300

    add: .evn

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -13,7 +13,7 @@ const Register = () => {
    const onFinish = async (values) => {
      setLoading(true);
      try {
-       const res = await fetch("http://localhost:5002/api/auth/register", {
+       const res = await fetch(process.env.REACT_APP_SERVER_URL+"/api/auth/register", {
          method: "POST",
          body: JSON.stringify(values),
          headers: { "Content-type": "application/json; charset=UTF-8" },
